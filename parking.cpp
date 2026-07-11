50dc5169b60139cc2200ac381ddbccdee2abcccb
user.name like : Insiya <zaidiinsiya83@gmail.com>
2026-07-11 16:47:33 +0530

    2d input

---
+++ parking.cpp
@@ -0,0 +1,28 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ int main(){
+    int row ;
+    cin>>row;
+    int col;
+    cin>>col;
+    int arr[row][col];
+    int maxone =0;
+int ans =INT_MIN;
+int maxrow =0;
+    for(int i =0; i<row;i++){
+         maxone =0;
+        for(int j = 0; j<col;j++){
+            cin>>arr[i][j]; // taking input row1 fill hogi then row2 
+        if(arr[i][j]==1){
+            maxone++;
+           }    
+        }
+        if(maxone>ans){
+    ans = maxone;
+maxrow = i+1;
+}
+    }
+
+cout << maxrow;
+  
+ }
